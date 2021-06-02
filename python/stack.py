d8dbea65f71a40edc21658157cd1fc76f02262a9
Won Yang <won@gatech.edu>
2021-06-02 08:50:51 -0500

    implements stack

--- python/stack.py
+++ python/stack.py
@@ -1,14 +1,19 @@
 class Stack:
-  # write your __init__ method here that should store a 'total' value which is the total number of elements in the Stack and a 'stack' value which is an array of stored values in the Stack
+    # write your __init__ method here that should store a 'total' value which is the total number of elements in the Stack and a 'stack' value which is an array of stored values in the Stack
+    def __init__(self):
+        self.total = 0
+        self.stack = []
 
-  def push(self):
-    # write your code to add data following LIFO and return the Stack
-    pass
+    def push(self, data):
+        # write your code to add data following LIFO and return the Stack
+        self.stack.append(data)
+        return self.stack
 
-  def pop(self, data):
-    # write your code to removes the data following LIFO and return the Stack
-    pass
+    def pop(self):
+        # write your code to removes the data following LIFO and return the Stack
+        self.stack.pop()
+        return self.stack
 
-  def size(self):
-    # write your code that returns the size of the Stack
-    pass
+    def size(self):
+        # write your code that returns the size of the Stack
+        return len(self.stack)
